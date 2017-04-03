b0ee5aa444d1dcfd57abc3e1f17969e1fb4c0994
Sergio Infante <raulsergio9@gmail.com>
2017-04-03 10:15:19 -0500

    Divide production and local settings in order to publish API

--- BxEvents/settings/base.py
+++ BxEvents/settings/base.py
@@ -19,11 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '33@^f0=57@&!z&5qy=u876rf5@9&i7$74ar6af^v%gxlq130sd'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -77,10 +72,7 @@ WSGI_APPLICATION = 'BxEvents.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': {}
 }
 
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn BxEvents.wsgi --log-file -
