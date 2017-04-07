a5c780e154cea13c64339ed4c671ee59a2230474
Sergio Infante <rsinfante@gmail.com>
2017-04-07 12:05:09 -0500

    Add employee qr code to endpoint response

--- employees/serializers.py
+++ employees/serializers.py
@@ -3,6 +3,13 @@ from .models import Employee
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    GOOGLE_QR_CHART_URL = 'https://chart.apis.google.com/chart?cht=qr&chs=500x500&chl='
+
+    employee_qr_code = serializers.SerializerMethodField('qr_code')
+
+    def qr_code(self, employee):
+        return self.GOOGLE_QR_CHART_URL + str(employee.pk)
+
     class Meta(object):
         model = Employee
-        fields = '__all__'
+        fields = ('id', 'name', 'avatar', 'email', 'role', 'twitter', 'github', 'website', 'employee_qr_code')
