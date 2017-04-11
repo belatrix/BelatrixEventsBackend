780db66d095ece9c2dead56f1ba5433e16e761e5
Sergio Infante <raulsergio9@gmail.com>
2017-04-11 16:40:55 -0500

    Add get all messages endpoint

--- notifications/models.py
+++ notifications/models.py
@@ -7,6 +7,8 @@ from django.utils.encoding import python_2_unicode_compatible
 @python_2_unicode_compatible
 class Message(models.Model):
     text = models.CharField(max_length=140)
+    datetime = models.DateTimeField(auto_now_add=True)
+    city = models.IntegerField(default=0)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
--- notifications/urls.py
+++ notifications/urls.py
@@ -1,7 +1,8 @@
-from .views import send_message_by_city
+from .views import send_message_by_city, get_all_messages
 from django.conf.urls import url
 
 
 urlpatterns = [
     url(r'^send/city/(?P<city_id>\d+)/$', send_message_by_city, name='send_message_by_city'),
+    url(r'^all/$', get_all_messages, name='get_all_messages'),
 ]
