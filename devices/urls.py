d94de1740c8a3f7635923aaf45a0d89e322f451e
Sergio Infante <raulsergio9@gmail.com>
2017-04-11 15:19:00 -0500

    Add update device city endpoint

--- devices/urls.py
+++ devices/urls.py
@@ -1,7 +1,8 @@
-from .views import android_device_registration
+from .views import android_device_registration, update_device_city
 from django.conf.urls import url
 
 
 urlpatterns = [
     url(r'^register/android/$', android_device_registration, name='android_registration'),
+    url(r'^update/city/$', update_device_city, name='update_device_city'),
 ]
--- devices/views.py
+++ devices/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -21,3 +22,17 @@ def android_device_registration(request):
         device = Device.objects.get(device_code=device_code)
         serializer = DeviceSerializer(device)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['PATCH', ])
+def update_device_city(request):
+    """
+    Update device city
+    """
+    device_code = (request.data['device_code'] if 'device_code' in request.data.keys() else None)
+    city = (request.data['city'] if 'city' in request.data.keys() else 0)
+    device = get_object_or_404(Device, device_code=device_code)
+    device.city = int(city)
+    device.save()
+    serializer = DeviceSerializer(device)
+    return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
