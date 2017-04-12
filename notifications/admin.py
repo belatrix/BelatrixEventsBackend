22b4053000d9b9f4aa2b8b615421f92fd90c4e14
Sergio Infante <raulsergio9@gmail.com>
2017-04-12 08:48:19 -0500

    Add send push notification function in message admin form

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import Message
+from utils.send_messages import send_push_notification
+from devices.models import Device
 
 
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('text', 'is_active')
+    list_display = ('text', 'is_active', 'city')
+
+    def save_model(self, request, obj, form, change):
+        devices = Device.objects.filter(city=obj.city)
+        send_push_notification(devices, obj.text)
+        obj.save()
 
 
 admin.site.register(Message, MessageAdmin)
