5c61d73e1671130c0813961fd714262999609114
Sergio Infante <raulsergio9@gmail.com>
2017-05-15 16:04:34 -0500

    Add send message endpoint to all, and modify pre-existing endpoints to send messages for those who haven’t register city when mobile app launches at the first time, they are going to receive all messages without city filter.

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,8 +1,9 @@
-from .views import send_message_by_city, get_all_messages
+from .views import send_message_by_city, get_all_messages, send_message_to_all
 from django.conf.urls import url
 
 
 urlpatterns = [
+    url(r'^send/all/$', send_message_to_all, name='send_message_to_all'),
     url(r'^send/city/(?P<city_id>\d+)/$', send_message_by_city, name='send_message_by_city'),
     url(r'^all/$', get_all_messages, name='get_all_messages'),
 ]
