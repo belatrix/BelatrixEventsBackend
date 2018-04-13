b3d11797be1f8d96a2c5bae1c5d0bfa1d479819a
Sergio Infante <raulsergio9@gmail.com>
2018-04-13 18:30:44 -0500

    Add endpoint to create user with email address.

--- participants/urls.py
+++ participants/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import user_detail
+from .views import user_detail, user_creation
 
 
 urlpatterns = [
     url(r'^(?P<user_id>\d+)/$', user_detail, name='user_detail'),
+    url(r'^create/$', user_creation, name='user_creation'),
 ]
--- participants/views.py
+++ participants/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from re import match as regex_match
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -14,3 +15,30 @@ def user_detail(request, user_id):
     user = get_object_or_404(User, pk=user_id)
     serializer = UserSerializer(user)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST', ])
+def user_creation(request):
+    if request.method == 'POST':
+        email = request.data['email'].lower()
+
+        if not regex_match(r"[^@]+@[^@]+\.[^@]+", email):
+            content = {'detail: Correo invalido'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+        random_password = User.objects.make_random_password(length=4, allowed_chars='hacktrx23456789')
+        subject = "Usuario creado para la Hackatrix"
+        message = "Su clave temporal, que debe cambiar es: %s" % (random_password)
+
+        try:
+            new_user = User.objects.create_user(email, password=random_password)
+            new_user.save()
+        except Exception as e:
+            print(e)
+            content = {'detail: Correo ya registrado'}
+            return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+        #TODO: Implementing email sending flow
+
+        content = {'detail: Correo registrado correctamente'}
+        return Response(content, status=status.HTTP_201_CREATED)
