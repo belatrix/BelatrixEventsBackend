ad0b969b4b3a79f1175b6c762b057d04fd5910ee
Sergio Infante <raulsergio9@gmail.com>
2018-04-17 08:43:01 -0500

    Modify urlpattern for recovery confirmation in order to avoid Pep8 E501

--- participants/urls.py
+++ participants/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     url(r'^authenticate/', CustomAuthToken.as_view()),
     url(r'^create/$', user_creation, name='user_creation'),
     url(r'^recover/$', user_password_recovery_request, name='user_password_recovery_request'),
-    url(r'^recover/(?P<user_uuid>[0-9a-z-]+)$', user_password_recovery_confirmation, name='user_password_recovery_confirmation'),
+    url(r'^recover/(?P<user_uuid>[0-9a-z-]+)$',
+        user_password_recovery_confirmation,
+        name='user_password_recovery_confirmation'),
 ]
