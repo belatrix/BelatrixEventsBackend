b5cad1de61ab0c0e7979bae8d7cdb48e083a5e35
Sergio Infante <raulsergio9@gmail.com>
2018-04-28 18:37:24 -0500

    Add IsParticipant permission

--- participants/permissions.py
+++ participants/permissions.py
@@ -1,8 +1,19 @@
 from rest_framework import permissions
 
+from .models import Participant
+
 
 class IsJury(permissions.BasePermission):
     message = 'Jury restricted'
 
     def has_permission(self, request, view):
         return request.user and request.user.is_jury
+
+
+class IsParticipant(permissions.BasePermission):
+    message = 'User needs to be a participant registered.'
+
+    def has_permission(self, request, view):
+        participants = Participant.objects.filter(email=request.user.email).count()
+        if participants > 0:
+            return True
