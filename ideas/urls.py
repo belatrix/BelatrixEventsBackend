69b02441d10e624819d7e73ecefaaafddab6a13d
Sergio Infante <raulsergio9@gmail.com>
2018-04-28 21:50:02 -0500

    Comment legacy idea vote url

--- ideas/urls.py
+++ ideas/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from .views import idea, idea_create, idea_register, idea_participants, idea_unregister
-from .views import idea_vote, idea_completed, idea_open
+from .views import idea_completed, idea_open
 
 
 urlpatterns = [
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^(?P<idea_id>\d+)/register/$', idea_register, name='idea_register'),
     url(r'^(?P<idea_id>\d+)/unregister/$', idea_unregister, name='idea_unregister'),
     url(r'^(?P<idea_id>\d+)/participants/$', idea_participants, name='idea_participants'),
-    url(r'^(?P<idea_id>\d+)/vote/$', idea_vote, name='idea_vote'),
+    # url(r'^(?P<idea_id>\d+)/vote/$', idea_vote, name='idea_vote'),
     url(r'^create/$', idea_create, name='idea_create'),
     # url(r'^list/event/(?P<event_id>\d+)/$', idea_list, name='idea_list'),
 ]
