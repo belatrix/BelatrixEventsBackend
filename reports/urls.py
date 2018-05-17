f1f8a1d29cec1f8248c27947d553a2050eda3e48
Sergio Infante <raulsergio9@gmail.com>
2018-05-17 14:08:34 -0500

    Add report for valid idea list

--- reports/urls.py
+++ reports/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
 from .views import index
+from .views import idea_list
 from .views import participants_idea
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^participants/idea/$', participants_idea, name='participants_idea'),
+    url(r'^event/(?P<event_id>\d+)/idea/list/$', idea_list, name='idea_list'),
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from ideas.models import IdeaParticipant
+from events.models import Event
+from ideas.models import Idea, IdeaParticipant
 
 
 def index(request):
@@ -11,3 +12,10 @@ def participants_idea(request):
     participants_list = IdeaParticipant.objects.all()
     context = {'participants_list': participants_list}
     return render(request, 'participants_idea.html', context)
+
+
+def idea_list(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    list = Idea.objects.filter(event=event, is_valid=True)
+    context = {'idea_list': list}
+    return render(request, 'idea_list.html', context)
