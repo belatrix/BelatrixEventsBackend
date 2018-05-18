988b978255d19c8af3e751d1962f6015fc6744de
Sergio Infante <raulsergio9@gmail.com>
2018-05-18 11:27:40 -0500

    Add meetings attendance reports

--- reports/views.py
+++ reports/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from events.models import Event
+from events.models import Attendance, Event, Meeting
 from ideas.models import Idea, IdeaParticipant
 
 
@@ -19,3 +19,15 @@ def idea_list(request, event_id):
     list = Idea.objects.filter(event=event, is_valid=True)
     context = {'idea_list': list}
     return render(request, 'idea_list.html', context)
+
+
+def meeting_list(request):
+    meetings = Meeting.objects.all()
+    context = {'meeting_list': meetings}
+    return render(request, 'meeting_list.html', context)
+
+
+def meeting_attendance_detail(request, meeting_id):
+    attendance_list = Attendance.objects.filter(meeting=meeting_id)
+    context = {'attendance_list': attendance_list}
+    return render(request, 'attendance_list.html', context)
