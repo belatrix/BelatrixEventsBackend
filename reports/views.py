9f728cb8ce70321c654505bb4f974afed2a82f48
Sergio Infante <raulsergio9@gmail.com>
2018-05-19 12:28:19 -0500

    Add event detail report

--- reports/views.py
+++ reports/views.py
@@ -1,12 +1,31 @@
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 
-from events.models import Attendance, Event, Meeting
+from events.models import Attendance, Event, Meeting, EventParticipant
 from ideas.models import Idea, IdeaParticipant, IdeaVotes, IdeaScores
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def event_information(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    participants_registered = EventParticipant.objects.filter(event=event)
+    attendances = Attendance.objects.filter(meeting__event=event).values('meeting__name').annotate(
+        total=Count('participant')).order_by('total')
+    ideas_proposed = Idea.objects.filter(event=event)
+    ideas_valid = Idea.objects.filter(event=event, is_valid=True)
+    team_members = IdeaParticipant.objects.filter(idea__event=event)
+    votes = IdeaVotes.objects.filter(event=event)
+    context = {'event': event,
+               'participants_registered': participants_registered,
+               'attendances': attendances,
+               'ideas_proposed': ideas_proposed,
+               'ideas_valid': ideas_valid,
+               'team_members': team_members,
+               'votes': votes}
+    return render(request, 'event.html', context)
 
 
 def participants_idea(request):
